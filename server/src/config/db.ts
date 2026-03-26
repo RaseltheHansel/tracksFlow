5aafa28120d4dba50fe671ca8f954b9fc76ccd77
RaseltheHansel <jraselthehansel@gmail.com>
2026-03-26 14:11:43 +0800

    done db ts

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -1,1 +1,20 @@
-asasdasdasd
+import { Sequelize} from 'sequelize';
+import dotenv from 'dotenv';
+dotenv.config();
+
+export const sequelize = new Sequelize({
+    dialect: "postgres",
+    host: process.env.DB_HOST || "localhost",
+    port: parseInt(process.env.DB_PORT || "5432" ),
+    database: process.env.DB_NAME || "trackflow",
+    username: process.env.DB_USER || "postgres",
+    password: process.env.DB_PASSWORD || "password",
+    logging: false,
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+    },
+
+});
