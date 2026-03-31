a9c67a9acd39eac97c49cd9b5feb87e380bf760b
RaseltheHansel <jraselthehansel@gmail.com>
2026-03-31 13:55:04 +0800

    done fixing cors

--- server/src/routes/collectRoutes.ts
+++ server/src/routes/collectRoutes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { collect } from "../controllers/collectController";
+import { collectCors } from "../middleware/cors";
 
 const router = Router();
 
-router.options('/', (req, res) => {
-    res.status(200).end();
+router.options('/', collectCors, (req, res) => {
+    res.status(204).end();
 });
 
-router.post('/', collect);
+router.post('/', collectCors, collect);
 
 export default router;
